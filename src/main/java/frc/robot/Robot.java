37d4ee391f4b8699f34a8641d158d87b07455df1
Puginator5000 <emmaminime3662@gmail.com>
2026-04-13 16:53:29 -0700

    homework

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,8 +1,11 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.xrp.XRPMotor;
+import edu.wpi.first.wpilibj.xrp.XRPServo;
 
 
 public class Robot extends TimedRobot {
@@ -11,19 +14,30 @@ public class Robot extends TimedRobot {
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
- 
- 
+  
+  private XRPMotor m_leftMotor = new XRPMotor(0);
+  private XRPMotor m_rightMotor = new XRPMotor(1);
+  //create encoders here
+  private XRPServo m_servo = new XRPServo(4);
+
   //constructor
   public Robot() {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+    
+    m_rightMotor.setInverted(true);
+  }
+
+  public void drive(double speed){  
+    m_leftMotor.set(speed);
+    m_rightMotor.set(speed);
   }
 
   @Override
   public void robotPeriodic() {}
 
-  
+
   @Override
   public void autonomousInit() {
     m_autoSelected = m_chooser.getSelected();
@@ -47,25 +61,12 @@ public class Robot extends TimedRobot {
 
   /** This function is called once when teleop is enabled. */
   @Override
-  public void teleopInit() {}
+  public void teleopInit() {
+    drive(.23);
+    m_servo.setAngle(270);
+  }
 
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {}
-
-  /** This function is called once when the robot is disabled. */
-  @Override
-  public void disabledInit() {}
-
-  /** This function is called periodically when disabled. */
-  @Override
-  public void disabledPeriodic() {}
-
-  /** This function is called once when test mode is enabled. */
-  @Override
-  public void testInit() {}
-
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {}
 }
